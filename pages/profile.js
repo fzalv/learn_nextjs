b0e24dad809cc49acb74185886a0544c6e12ef17
fzalv <youhaveamessag3@gmail.com>
2023-12-16 14:12:22 +0700

    Materi hari 5

---
+++ pages/profile.js
@@ -0,0 +1,17 @@
+import Layout from "@/layout";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import Content from "@/components/content";
+
+export default function profile() {
+  return (
+    <>
+      <Layout>
+        <p>Profile</p>
+      </Layout>
+      {/* <Header />
+      <p>Profiles</p>
+      <Footer /> */}
+    </>
+  );
+}
