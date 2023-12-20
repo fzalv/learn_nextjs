f07a8fbbc061b8f991d0b6ad7d06d5170ed88c8d
fzalv <youhaveamessag3@gmail.com>
2023-12-20 22:11:59 +0700

    Materi hari 7

--- components/header/index.js
+++ components/header/index.js
@@ -1,6 +1,21 @@
+import Link from "next/link";
 import styles from "./style.module.css";
 
 function Header() {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={styles.header}>
+      <ul>
+        <li>
+          <Link href="">Home</Link>
+        </li>
+        <li>
+          <Link href="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link href="/users">Users</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 export default Header;
