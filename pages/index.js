b859159f4d3346f9be6f44dd40a68742429d0c43
fzalv <youhaveamessag3@gmail.com>
2023-12-21 22:27:02 +0700

    Materi hari 9

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import Layout from "@/layout";
+import dynamic from "next/dynamic";
+import { useEffect } from "react";
 
-export default function Main({ children }) {
+const layoutComponent = dynamic(() => import("@/layout"));
+
+export default function Main() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("response => ", res))
+      .catch((err) => console.log("err => ", err));
+  }, []);
+
   return (
     <>
       <Layout metaTitle="Home">
