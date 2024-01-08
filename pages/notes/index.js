94e9fec5b8fa77b9eeaaa3607d9720325af69bd9
fzalv <youhaveamessag3@gmail.com>
2024-01-08 23:30:29 +0700

    Modal

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -19,8 +19,16 @@ import {
   AlertDialogHeader,
   AlertDialogContent,
   AlertDialogOverlay,
-  AlertDialogCloseButton,
   useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Input,
+  Textarea,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { useRouter } from "next/router";
@@ -33,36 +41,18 @@ export default function Notes({ repo }) {
   console.log("data -> ", repo);
   const [notes, setNotes] = useState();
   const router = useRouter();
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    isOpen,
+    onOpen,
+    onClose,
+    isOpen2,
+    onOpen2,
+    onClose2,
+    isOpen3,
+    onOpen3,
+    onClose3,
+  } = useDisclosure();
   const cancelRef = React.useRef();
-
-  // const { data, isLoading } = useQueries({
-  //   prefixUrl: "https://dummyjson.com/products/",
-  // });
-
-  // const { data, isLoading } = useSWR(
-  //   "https://dummyjson.com/products/",
-  //   fetcher,
-  //   {
-  //     revalidateOnFocus: true,
-  //   }
-  // );
-
-  // useEffect(() => {
-  //   async function fetchingData() {
-  //     const res = await fetch("https://dummyjson.com/products");
-  //     const listNotes = await res.json();
-  //     setNotes(listNotes);
-  //   }
-  //   fetchingData();
-  // }, []);
-
-  // useEffect(() => {
-  //   fetch("/api/hello")
-  //     .then((res) => res.json())
-  //     .then((res) => console.log("response => ", res))
-  //     .catch((err) => console.log("err => ", err));
-  // }, []);
 
   const handleDelete = async (id) => {
     try {
@@ -81,13 +71,28 @@ export default function Notes({ repo }) {
     } catch (error) {}
   };
 
+  const handlesubmit = async () => {
+    const response = await mutate({
+      url: "https://dummyjson.com/products/add",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      payload: notes,
+    });
+
+    console.log("response => ", response);
+  };
+
   // console.log("notes => ", notes);
   return (
     <>
       <LayoutComponent metaTitle="Notes">
         <Box padding="5">
           <Flex justifyContent="end">
-            <Button colorScheme="blue" onClick={() => router.push("notes/add")}>
+            {/* <Button colorScheme="blue" onClick={() => router.push("notes/add")}>
+              Add Notes
+            </Button> */}
+            <Button colorScheme="blue" onClick={onOpen}>
               Add Notes
             </Button>
           </Flex>
@@ -103,23 +108,29 @@ export default function Notes({ repo }) {
                       <Text>{item.description}</Text>
                     </CardBody>
                     <CardFooter>
-                      <Button
+                      {/* <Button
                         onClick={() => router.push(`/notes/edit/${item?.id}`)}
+                        variant="solid"
+                        colorScheme="blue">
+                        Edit
+                      </Button> */}
+                      <Button
+                        onClick={onOpen2}
                         variant="solid"
                         colorScheme="blue">
                         Edit
                       </Button>
                       <Button
                         // onClick={() => handleDelete(item?.id)}
-                        onClick={onOpen}
+                        onClick={onOpen3}
                         variant="solid"
                         colorScheme="red">
                         Delete
                       </Button>
                       <AlertDialog
-                        isOpen={isOpen}
+                        isOpen={isOpen3}
                         leastDestructiveRef={cancelRef}
-                        onClose={onClose}>
+                        onClose={onClose3}>
                         <AlertDialogOverlay>
                           <AlertDialogContent>
                             <AlertDialogHeader fontSize="lg" fontWeight="bold">
@@ -132,7 +143,7 @@ export default function Notes({ repo }) {
                             </AlertDialogBody>
 
                             <AlertDialogFooter>
-                              <Button ref={cancelRef} onClick={onClose}>
+                              <Button ref={cancelRef} onClick={onClose3}>
                                 Cancel
                               </Button>
                               <Button
@@ -152,23 +163,103 @@ export default function Notes({ repo }) {
             </Grid>
           </Flex>
         </Box>
+
+        {/* MODAL ADD */}
+
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Card margin="5" padding="5">
+                <Heading>Add Notes</Heading>
+                <Grid gap="5">
+                  <GridItem>
+                    <Text>Title</Text>
+                    <Input
+                      type="text"
+                      onChange={(event) =>
+                        setNotes({ ...notes, title: event.target.value })
+                      }
+                    />
+                  </GridItem>
+                  <GridItem>
+                    <Text>Description</Text>
+                    <Textarea
+                      onChange={(event) =>
+                        setNotes({ ...notes, description: event.target.value })
+                      }
+                    />
+                  </GridItem>
+                  <GridItem>
+                    <Button colorScheme="blue" onClick={() => handlesubmit()}>
+                      Submit
+                    </Button>
+                  </GridItem>
+                </Grid>
+              </Card>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+
+        {/* MODAL EDIT */}
+        <Modal isOpen={isOpen2} onClose={onClose2}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Card margin="5" padding="5">
+                <Heading>Edit Notes</Heading>
+                <Grid gap="5">
+                  <GridItem>
+                    <Text>Title</Text>
+                    <Input
+                      type="text"
+                      value={notes?.title || ""}
+                      onChange={(event) =>
+                        setNotes({ ...notes, title: event.target.value })
+                      }
+                    />
+                  </GridItem>
+                  <GridItem>
+                    <Text>Description</Text>
+                    <Textarea
+                      value={notes?.description || ""}
+                      onChange={(event) =>
+                        setNotes({ ...notes, description: event.target.value })
+                      }
+                    />
+                  </GridItem>
+                  <GridItem>
+                    <Button colorScheme="blue" onClick={() => handlesubmit()}>
+                      Submit
+                    </Button>
+                  </GridItem>
+                </Grid>
+              </Card>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose2}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </LayoutComponent>
-      <p>tes</p>
+      {/* <p>tes</p> */}
     </>
   );
 }
-// export async function getStaticProps() {
-//   const res = await fetch("https://dummyjson.com/products");
-//   const note = await res.json();
-//   return { props: { note }, revalidate: 10 };
-// }
-
 export async function getServerSideProps() {
-  // Fetch data from external API
-  // const res = await fetch("../api/hello");
   const repo = await getData();
-  // console.log(repo);
-  // const repo = await res.json();
-  // Pass data to the page via props
   return { props: { repo } };
 }
